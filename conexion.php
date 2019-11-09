86b027658de3611fcebd9e273e1fa0e12ca961a5
SoyLuisReyes <rodolforeyes2016@outlook.es>
2019-11-09 16:21:56 -0600

    Version 1

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+
+$servidor = 'localhost';
+$usuario  = 'root';
+$password = '';
+$basededatos = 'experience'; 
+$db = mysqli_connect($servidor,$usuario,$password,$basededatos);
+
+mysqli_query ($db,"SET NAMES 'utf8'");
+
+// iniciar la sesion 
+if(!isset($_SESSION)){
+    session_start();  
+  }
+  
