daa372ff5e26fe0fa1fcbac7b049c98ad8fdda31
Luis Reyes <43224527+SoyLuisReyes@users.noreply.github.com>
2019-11-10 01:52:56 -0600

    Add files via upload
    Se ha diseñado el formulario de contacto del sitio web, aun falta programarlo con php

--- contact.php
+++ contact.php
@@ -1,91 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Jobstart &mdash; Colorlib Website Template</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!-- fin del cabecera -->
+<?php require_once 'include/cabecera.php';?>
 
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,300,400,700,900|Roboto+Mono:300,400,500"> 
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
-    <link rel="stylesheet" href="css/jquery-ui.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/animate.css">
-    
-    
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-    <link rel="stylesheet" href="css/fl-bigmug-line.css">
-  
-    <link rel="stylesheet" href="css/aos.css">
 
-    <link rel="stylesheet" href="css/style.css">
-    
-  </head>
-  <body>
-  
-  <div class="site-wrap">
-
-    <div class="site-mobile-menu">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div> <!-- .site-mobile-menu -->
-    
-    
-    <header class="site-navbar py-1" role="banner">
-
-      <div class="container">
-        <div class="row align-items-center">
-          
-          <div class="col-6 col-xl-2">
-            <h1 class="mb-0"><a href="index.html" class="text-black h2 mb-0">Job<strong>start</strong></a></h1>
-          </div>
-
-          <div class="col-10 col-xl-10 d-none d-xl-block">
-            <nav class="site-navigation text-right" role="navigation">
-
-              <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
-                <li><a href="index.html">Home</a></li>
-                <li class="has-children">
-                  <a href="category.html">Category</a>
-                  <ul class="dropdown">
-                    <li><a href="#">Full Time</a></li>
-                    <li><a href="#">Part Time</a></li>
-                    <li><a href="#">Freelance</a></li>
-                    <li><a href="#">Internship</a></li>
-                    <li><a href="#">Termporary</a></li>
-                  </ul>
-                </li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="new-post.html"><span class="rounded bg-primary py-2 px-3 text-white"><span class="h5 mr-2">+</span> Post a Job</span></a></li>
-              </ul>
-            </nav>
-          </div>
-
-          <div class="col-6 col-xl-2 text-right d-block">
-            
-            <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
-
-          </div>
-
-        </div>
-      </div>
-      
-    </header>
 
     <div class="unit-5 overlay" style="background-image: url('images/hero_bg_2.jpg');">
       <div class="container text-center">
-        <h2 class="mb-0">Contact</h2>
-        <p class="mb-0 unit-6"><a href="index.html">Home</a> <span class="sep">></span> <span>Contact</span></p>
+        <h2 class="mb-0">Contacto</h2>
+        <p class="mb-0 unit-6"><a href="index.html">Principal</a> <span class="sep">></span> <span>Contacto</span></p>
       </div>
     </div>
 
@@ -104,34 +26,34 @@
 
               <div class="row form-group">
                 <div class="col-md-12 mb-3 mb-md-0">
-                  <label class="font-weight-bold" for="fullname">Full Name</label>
-                  <input type="text" id="fullname" class="form-control" placeholder="Full Name">
+                  <label class="font-weight-bold" for="fullname">Nombre completo</label>
+                  <input type="text" id="fullname" class="form-control" placeholder="Nombre">
                 </div>
               </div>
               <div class="row form-group">
                 <div class="col-md-12">
-                  <label class="font-weight-bold" for="email">Email</label>
-                  <input type="email" id="email" class="form-control" placeholder="Email Address">
+                  <label class="font-weight-bold" for="email">Correo</label>
+                  <input type="email" id="email" class="form-control" placeholder="Correo">
                 </div>
               </div>
               <div class="row form-group">
                 <div class="col-md-12">
-                  <label class="font-weight-bold" for="email">Subject</label>
-                  <input type="text" id="subject" class="form-control" placeholder="Enter Subject">
+                  <label class="font-weight-bold" for="email">Tema</label>
+                  <input type="text" id="subject" class="form-control" placeholder="Tema del correo">
                 </div>
               </div>
               
 
               <div class="row form-group">
                 <div class="col-md-12">
-                  <label class="font-weight-bold" for="message">Message</label> 
-                  <textarea name="message" id="message" cols="30" rows="5" class="form-control" placeholder="Say hello to us"></textarea>
+                  <label class="font-weight-bold" for="message">Mensaje</label> 
+                  <textarea name="message" id="message" cols="30" rows="5" class="form-control" placeholder="Escriba su mensaje.."></textarea>
                 </div>
               </div>
 
               <div class="row form-group">
                 <div class="col-md-12">
-                  <input type="submit" value="Send" class="btn btn-primary  py-2 px-4">
+                  <input type="submit" value="Enviar" class="btn btn-primary  py-2 px-4">
                 </div>
               </div>
 
@@ -141,23 +63,23 @@
 
           <div class="col-lg-4">
             <div class="p-4 mb-3 bg-white">
-              <h3 class="h5 text-black mb-3">Contact Info</h3>
-              <p class="mb-0 font-weight-bold">Address</p>
+              <h3 class="h5 text-black mb-3">Informacion de contacto</h3>
+              <p class="mb-0 font-weight-bold">Correo</p>
               <p class="mb-4">203 Fake St. Mountain View, San Francisco, California, USA</p>
 
-              <p class="mb-0 font-weight-bold">Phone</p>
+              <p class="mb-0 font-weight-bold">telefono</p>
               <p class="mb-4"><a href="#">+1 232 3235 324</a></p>
 
-              <p class="mb-0 font-weight-bold">Email Address</p>
-              <p class="mb-0"><a href="#">youremail@domain.com</a></p>
+              <p class="mb-0 font-weight-bold">Direccion de correo</p>
+              <p class="mb-0"><a href="#">Expirence@pasantias.com</a></p>
 
             </div>
-            
+<!--             
             <div class="p-4 mb-3 bg-white">
               <h3 class="h5 text-black mb-3">More Info</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa ad iure porro mollitia architecto hic consequuntur. Distinctio nisi perferendis dolore, ipsa consectetur</p>
               <p><a href="#" class="btn btn-primary  py-2 px-4 rounded-0">Learn More</a></p>
-            </div>
+            </div> -->
           </div>
         </div>
       </div>
