75b9d9e43c47888164519cae81e25fb832f78b46
Luis Reyes <43224527+SoyLuisReyes@users.noreply.github.com>
2019-11-10 07:13:01 -0600

    Add files via upload

---
+++ principal.php
@@ -0,0 +1,81 @@
+  <!-- SECCION PRINCIPAL -->
+              <!-- Primera seccion  -->
+              <div class="job-post-item bg-white p-4 d-block d-md-flex align-items-center">
+       
+       <div class="mb-4 mb-md-0 mr-5">
+        <div class="job-post-item-header d-flex align-items-center">
+          <h2 class="mr-3 text-black h4">Desarrollador Web</h2>
+          <div class="badge-wrap">
+           <span class="bg-primary text-white badge py-2 px-4">Medio tiempo</span>
+          </div>
+        </div>
+        <div class="job-post-item-body d-block d-md-flex">
+          <div class="mr-3"><span class="fl-bigmug-line-portfolio23"></span> <a href="#">Ingeniero de sistemas</a></div>
+          <div>
+            <span class="fl-bigmug-line-big104"></span> <span>Carazo</span>
+            <span class="fl-bigmug-line-circular220">Empresa S.A</span>
+            <span class="fa-money"> 3000 </span>
+            <span><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+               </p></span>
+           </div>
+        </div>
+       </div>
+
+       <div class="ml-auto">
+         <a href="#" class="btn btn-secondary rounded-circle btn-favorite text-gray-500"><span class="icon-heart"></span></a>
+         <a href="job-single.php" class="btn btn-primary py-2">Aplicar al puesto</a>
+       </div>
+    </div>
+     <!-- Fin de la primera seccion  -->
+
+  </div>
+ </div>
+
+   <!-- Segunda seccion  -->
+ <div class="row" data-aos="fade">
+  <div class="col-md-12">
+    <div class="job-post-item bg-white p-4 d-block d-md-flex align-items-center">
+
+       <div class="mb-4 mb-md-0 mr-5">
+        <div class="job-post-item-header d-flex align-items-center">
+          <h2 class="mr-3 text-black h4">Formulacion y evaluacion de proyectos</h2>
+          <div class="badge-wrap">
+           <span class="bg-warning text-white badge py-2 px-4">tiempo completo</span>
+          </div>
+        </div>
+        <div class="job-post-item-body d-block d-md-flex">
+          <div class="mr-3"><span class="fl-bigmug-line-portfolio23"></span> <a href="#">Google, Inc.</a></div>
+         <div>
+            <span class="fl-bigmug-line-big104"></span> <span>Managua</span>
+             <span class="fl-bigmug-line-circular220">Empresa LALA</span>
+         </div>
+          
+        </div>
+       </div>
+
+       <div class="ml-auto">
+         <a href="#" class="btn btn-danger rounded-circle btn-favorite active"><span class="icon-heart"></span></a>
+         <a href="job-single.php" class="btn btn-primary py-2">Aplicar al puesto</a>
+       </div>
+
+    </div>
+  </div>
+ </div>
+ <!-- Fin de la segunda seccion  -->
+
+ <div class="row mt-5">
+          <div class="col-md-12 text-center">
+            <div class="site-block-27">
+              <ul>
+                <li><a href="#"><i class="icon-keyboard_arrow_left h5"></i></a></li>
+                <li class="active"><span>1</span></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#"><i class="icon-keyboard_arrow_right h5"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>    
