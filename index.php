3381558095d3110285d440b2ea00d75b11a209cc
SoyLuisReyes <rodolforeyes2016@outlook.es>
2019-11-09 18:40:20 -0600

    v

---
+++ index.php
@@ -0,0 +1,14 @@
+<!-- fin del cabecera -->
+<?php require_once 'include/cabecera.php';?>
+
+
+<!-- Header -->
+<?php require_once 'include/header.php';?>
+
+
+<!-- contenido principal -->
+<?php require_once 'include/principal.php';?>
+
+<!-- footer -->
+<?php require_once 'include/footer.php';?>
+ 
