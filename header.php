ee8b3e99f4b2415406d487d60900b48b0ff4331a
Luis Reyes <43224527+SoyLuisReyes@users.noreply.github.com>
2019-11-10 06:55:07 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,94 @@
+
+
+    <div class="site-blocks-cover" style="background-image: url(assets/images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+      <div class="container">
+        <div class="row row-custom align-items-center">
+          <div class="col-md-10">
+            <h1 class="mb-2 text-black w-75"><span class="font-weight-bold">Te acercamos</span> a tu proximo trabajo</h1>
+            <div class="job-search">
+              <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active py-3" id="pills-job-tab" data-toggle="pill" href="#pills-job" role="tab" aria-controls="pills-job" aria-selected="true">Buscar pasantia</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link py-3" id="pills-candidate-tab" data-toggle="pill" href="#pills-candidate" role="tab" aria-controls="pills-candidate" aria-selected="false">Candidatos</a>
+                </li>
+              </ul>
+              <div class="tab-content bg-white p-4 rounded" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-job" role="tabpanel" aria-labelledby="pills-job-tab">
+                  <form action="#" method="post">
+                    <div class="row">
+                      <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
+                        <input type="text" class="form-control" placeholder="Ej. Diseñador">
+                      </div>
+                      <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
+                        <div class="select-wrap">
+                          <span class="icon-keyboard_arrow_down arrow-down"></span>
+                          <select name="" id="" class="form-control">
+                            <option value="">Categoria</option>
+                            <option value="fulltime">Categoria 1</option>
+                            <option value="fulltime">Categoria 2</option>
+                            <!-- <option value="freelance">Freelance</option>
+                            <option value="internship">Internship</option>
+                            <option value="internship">Termporary</option> -->
+                          </select>
+                        </div>
+                      </div>
+                      <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
+                        <input type="text" class="form-control form-control-block search-input" id="autocomplete" placeholder="Departamento" onFocus="geolocate()">
+                      </div>
+                      <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
+                        <input type="submit" class="btn btn-primary btn-block" value="Buscar">
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <div class="tab-pane fade" id="pills-candidate" role="tabpanel" aria-labelledby="pills-candidate-tab">
+                  <form action="#" method="post">
+                    <div class="row">
+                      <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
+                        <input type="text" class="form-control" placeholder="Nombre">
+                      </div>
+                      <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
+                        <div class="select-wrap">
+                          <span class="icon-keyboard_arrow_down arrow-down"></span>
+                          <select name="" id="" class="form-control">
+                            <option value="">Categoria</option>
+                            <option value="fulltime">Categoria 1</option>
+                            <option value="fulltime">Categoria 1</option>
+                            <!-- <option value="freelance">Freelance</option>
+                            <option value="internship">Internship</option>
+                            <option value="internship">Termporary</option> -->
+                          </select>
+                        </div>
+                      </div>
+                      <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
+                        <input type="text" class="form-control form-control-block search-input" id="autocomplete" placeholder="Ubicacion" onFocus="geolocate()">
+                      </div>
+                      <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
+                        <input type="submit" class="btn btn-primary btn-block" value="Buscar">
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>  
+    
+
+    <div class="site-section bg-light">
+      <div class="container">
+        <div class="row justify-content-start text-left mb-5">
+          <div class="col-md-9" data-aos="fade">
+            <h2 class="font-weight-bold text-black">Ofertas recientes</h2>
+          </div>
+          <!-- <div class="col-md-3" data-aos="fade" data-aos-delay="200">
+            <a href="#" class="btn btn-primary py-3 btn-block"><span class="h5">+</span> Post a Job</a>
+          </div> -->
+        </div>
+
+        <div class="row" data-aos="fade">
+         <div class="col-md-12">
